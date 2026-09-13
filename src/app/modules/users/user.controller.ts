3fc4e3a927a6046a5c8b820a68478bb28ffb0fbf
Sakib Fakir <FakirSakib22232@gmail.com>
2026-09-13 21:54:26 +0800

    feat(user) add create user controller

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,38 +1,55 @@
-import type { Response,Request, NextFunction } from "express";
+import type { Response, Request, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import { ZodError } from "zod";
+import { createUserSchema } from "./user.validation.js";
+import {  DB } from "../../../../prisma/db/prisma.db.js";
+import { StatusCodes } from "http-status-codes";
 
-
-
-
-
-const createUser = (req:Request , res:Response,next:NextFunction)=>{
-
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        
-        
+        const data = createUserSchema.parse(req.body);
+
+        const isUserExits = await DB.User.first({
+            email:data.email
+        })
+        if(isUserExits){
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                "success":false,
+                "message":"Please  provide another email"
+            })
+        }
+
+
+
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+
+        const user = await DB.User.create({
+            name: data.name,
+            fullName: data.fullName,
+            email: data.email,
+            country: data.country,
+            password: hashedPassword,
+            age: data.age ?? null,
+            avatarUrl: data.avatarUrl ?? null,
+            bio: data.bio ?? null,
+        });
+         console.log(user, "CREATED")
+         
+        const { password, ...userWithoutPassword } = user;
+
+        res.status(201).json(userWithoutPassword);
     } catch (error) {
+        if (error instanceof ZodError) {
+            res.status(400).json({ message: "Validation failed" });
+            return;
+        }
+
         next(error);
-        
     }
-    
 };
 
 
-const updateUser = ()=>{};
-const deleteUser = ()=>{}
-const getUser    = ()=>{};
-
-
-
-
-
-
-
-
-
-
-
-
 export const userController = {
-    createUser,deleteUser,getUser,updateUser
+    createUser,
 
-}
+};
---
+++ src/app/modules/users/user.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { userController } from "./user.controller.js";
+
+
+
+const router = Router();
+
+
+router.post('/', userController.createUser);
+
+
+export const userRouter  = router;
