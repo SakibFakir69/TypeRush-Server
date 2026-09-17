663192423609dea299b6e133316e42bdd3e9bf9b
Sakib Fakir <FakirSakib22232@gmail.com>
2026-09-17 22:22:40 +0800

    feat(user) add update user controller

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,24 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import type { Response, Request, NextFunction } from "express";
 import bcrypt from "bcrypt";
 import { ZodError } from "zod";
 import { createUserSchema, type User } from "./user.validation.js";
-import {  DB } from "../../../../prisma/db/prisma.db.js";
-import { getStatusCode, getStatusText, StatusCodes } from "http-status-codes";
+import { DB } from "../../../../prisma/db/prisma.db.js";
+import { StatusCodes } from "http-status-codes";
 import { returnResponse } from "../../../helpers/return-response.js";
+
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const data = createUserSchema.parse(req.body);
 
         const isUserExits = await DB.User.first({
-            email:data.email
+            email: data.email
         })
-        if(isUserExits){
-           
-            return returnResponse(res,false,StatusCodes.BAD_REQUEST,"Please  provide another email")
+        if (isUserExits) {
+
+            return returnResponse(res, false, StatusCodes.BAD_REQUEST, "Please  provide another email")
         }
-
-
 
         const hashedPassword = await bcrypt.hash(data.password, 10);
 
@@ -32,12 +32,12 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
             avatarUrl: data.avatarUrl ?? null,
             bio: data.bio ?? null,
         });
-         console.log(user, "CREATED")
+        console.log(user, "CREATED")
 
         const { password, ...userWithoutPassword } = user;
 
-        
-       return returnResponse(res,true,StatusCodes.CREATED,"User created successfully",userWithoutPassword)
+
+        return returnResponse(res, true, StatusCodes.CREATED, "User created successfully", userWithoutPassword)
 
     } catch (error) {
         if (error instanceof ZodError) {
@@ -52,7 +52,35 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
 
 
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.user?.userId as string;
+        const { bio, avatarUrl, country, fullName, name } = req.body;
+
+        if (!userId) {
+            return returnResponse(res, false, StatusCodes.UNAUTHORIZED, "You are unauthorized");
+        }
+
+        const data: Record<string, unknown> = {};
+        if (name !== undefined) data.name = name;
+        if (avatarUrl !== undefined) data.avatarUrl = avatarUrl;
+        if (country !== undefined) data.country = country;
+        if (fullName !== undefined) data.fullName = fullName;
+        if (bio !== undefined) data.bio = bio;
+
+        if (Object.keys(data).length === 0) {
+            return returnResponse(res, false, StatusCodes.BAD_REQUEST, "No fields provided to update");
+        };
+
+        const updatedUser = await DB.User.where({ id: userId }).update(data);
+
+        return returnResponse(res, true, StatusCodes.OK, "Profile updated successfully", updatedUser);
+    } catch (error) {
+        next(error);
+    }
+};
+
 export const userController = {
-    createUser,
+    createUser,updateUser
 
 };
