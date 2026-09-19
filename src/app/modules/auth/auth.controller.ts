e7384ae257aa2dbef96f51164e6bd2be88e7921a
Sakib Fakir <FakirSakib22232@gmail.com>
2026-09-19 15:47:28 +0800

    feat(middleware) add verifyToken middleware

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -7,56 +7,52 @@ import { DB } from "../../../../prisma/db/prisma.db.js";
 import jwt from "jsonwebtoken"
 import { setCookies } from "../../../helpers/set-cookies.js";
 
-const userLogin =  async (req: Request, res: Response, next: NextFunction) => {
+const userLogin = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        const {email,password}=req.body as  {
-            email:string,
-            password:string
+        const { email, password } = req.body as {
+            email: string,
+            password: string
         };
 
-        if(!email){
-            return returnResponse(res,true,StatusCodes.BAD_REQUEST, "Please provide email")
+        if (!email) {
+            return returnResponse(res, true, StatusCodes.BAD_REQUEST, "Please provide email")
         }
-        if(!password){
-              return returnResponse(res,true,StatusCodes.BAD_REQUEST, "Please provide password")
+        if (!password) {
+            return returnResponse(res, true, StatusCodes.BAD_REQUEST, "Please provide password")
         }
-        const hashPassword = await DB.User.first({email:email});
+        const hashPassword = await DB.User.first({ email: email });
 
-        const isCorrectPassword = await bcrypt.compare(password,hashPassword?.password as string);
-        if(!isCorrectPassword){
-            return returnResponse(res,true,StatusCodes.BAD_REQUEST, "Please provide correct password");
+        const isCorrectPassword = await bcrypt.compare(password, hashPassword?.password as string);
+        if (!isCorrectPassword) {
+            return returnResponse(res, true, StatusCodes.BAD_REQUEST, "Please provide correct password");
         }
 
-        const payload={
-            userId:hashPassword?.id,
-            email:hashPassword?.email,
-            name:hashPassword?.name
+        const payload = {
+            userId: hashPassword?.id,
+            email: hashPassword?.email,
+            name: hashPassword?.name
 
         }
-        const accessToken=jwt.sign(payload, "mysecrectkey", {
-            expiresIn:"30m"
+        const accessToken = jwt.sign(payload, process.env.JWT_SECRET_FOR_ACCESS_TOKEN as string, {
+            expiresIn: "15m"
         })
-        const refreshToken=jwt.sign(payload, "mysecrectkey", {
-            expiresIn:"30d"
+        const refreshToken = jwt.sign(payload,process.env.JWT_SECRET_FOR_REFRESH_TOKEN as string , {
+            expiresIn: "15d",
+        
         })
         const data = {
-            accessToken:accessToken,
-            refreshToken:refreshToken
+            accessToken: accessToken,
+            refreshToken: refreshToken
         }
 
-        setCookies(res, "accessToken",accessToken, 15 * 60 * 1000);
-         setCookies(res, "refreshToken",refreshToken, 15 * 60 * 1000);
+        setCookies(res, "accessToken", accessToken, 15 * 60 * 1000);
+        setCookies(res, "refreshToken", refreshToken, 15 * 24 * 60 * 60 * 1000);
+        return returnResponse(res, true, StatusCodes.OK, "User Login successfully", data);
 
-        
-
-        
-
-        return returnResponse(res, true,StatusCodes.OK, "User Login successfully", data);
-        
     } catch (error) {
         next(error);
-        
+
     }
 
 }
